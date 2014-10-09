c6ab80b3684b0e2d986a280d0e10a35eca8741a2
hidykong <hidykong@gmail.com>
2014-10-09 12:17:32 -0500

    dynamic number of gazes

--- circle/circle.js
+++ circle/circle.js
@@ -1,10 +1,10 @@
 var width = 960,
-    height = 400;
+    height = 450;
 var color = ["#6797C6", "#6797C6", "#F78A3C", "#F6749E"];
 var sliderWidth = 500;
-var graphHeight = 350;
+var graphHeight = 450;
 
-
+var interaction = [{"exObj":14 ,"chObj":14, "exCh": 15, "chEx": 26}];
 
 //start code
 var svg = d3.select("body").append("svg")
@@ -18,76 +18,74 @@ d3.json("test-2.json", function(error, graph) {
 		    .attr("width", sliderWidth)
 		    .attr("height", graphHeight);
 
-
+	middleLine = height /2; //the middle line
+	radius = 20;
 	  var node = svg.selectAll(".node")
 	      .data(graph.nodes)
 	    .enter().append("circle")
 	      .attr("class", "node")
-	      .attr("r", 20)
+	      .attr("r", radius)
 	      .style("fill", function(d) { return color[d.group]; })
 	      .attr("cx", function(d) { return d.x; })
-	        .attr("cy", function(d) { return d.y; });
+	        .attr("cy", middleLine);
 	
 	svg.selectAll(".text")
 		.data(graph.nodes)
 		.enter().append("text")
 		.attr("class", "text")
       .attr("dx", function(d){return d.x - 10;})
-      .attr("dy", function(d){return d.y + 5;})
+      .attr("dy", middleLine+ 5)
       .attr("fill", "#F7F6F5")
       .text(function(d) { return d.name });	
 
-	start = 200
 
-//ex-obj
+	y = middleLine - radius;
+	y2 = middleLine + radius;
+
 	exX = 100;
-	y = 180;
 	obX = 300;
 	chX = 500;
-	y2 = 220;
 
-	svg.append("path")
-		.attr("d", "M" + exX + " " + y + " " + "C 150 150, 250 150," 
-			+ obX + " " + y )
-	    .attr("class", "exLine");
-	
-	svg.append("path")
-		.attr("d", "M" + exX + " " + y + " " + "C 125 100, 275 100," 
-			+ obX + " " + y )
-	    .attr("class", "exLine");
+	//ex-obj
+	exObj = interaction[0]["exObj"];
+	for (i = 0; i < exObj ; i++) { 
+		svg.append("path")
+			.attr("d", "M" + exX + " " + y + " " 
+				+ "C 150 " + (150 - 50/exObj*i)+ ", 250 " + (150 - 50/exObj*i)+ "," 
+				+ obX + " " + y )
+		    .attr("class", "exLine");
+	}
 
 	//ex-ch
-	svg.append("path")
-		.attr("d", "M" + exX + " " + y + " " + "C 150 20, 450 20," 
-			+ chX + " " + y )
-	    .attr("class", "exLine");	
+	exCh = interaction[0]["exCh"];
+	for (i = 0; i < interaction[0]["exCh"] ; i++) { 
+		svg.append("path")
+			.attr("d", "M" + exX + " " + y + " " 
+				+ "C 150 " + (70 - 80/exCh*i)+ ", 450 " + (70 - 80/exCh*i) + "," 
+				+ chX + " " + y )
+		    .attr("class", "exLine");
+	}
 
+	//ch-obj	
 
-//ch-obj	
+	chObj = interaction[0]["chObj"];
+	for (i = 0; i < interaction[0]["chObj"] ; i++) { 
+		svg.append("path")
+			.attr("d", "M" + obX + " " + y2 + " " 
+				+ "C 350 " + (320 - 50/chObj*i)+ ", 450 " + (320 - 50/chObj*i)+ "," 
+				+ chX + " " + y2 )
+		    .attr("class", "chLine");
+	}	    
 
-	svg.append("path")
-		.attr("d", "M" + obX + " " + y2 + " " 
-			+ "C " + (obX + 20) + " 280, " + (chX - 20) + " 280, " 
-			+ chX + " " + y2 )
-	    .attr("class", "chLine");		
-
-	svg.append("path")
-		.attr("d", "M" + obX + " " + y2 + " " 
-			+ "C " + (obX + 10) + " 300, " + (chX - 10) + " 300, " 
-			+ chX + " " + y2 )
-	    .attr("class", "chLine");	
-
-	svg.append("path")
-		.attr("d", "M" + obX + " " + y2 + " " 
-			+ "C " + (obX + 20) + " 260, " + (chX - 20) + " 260, " 
-			+ chX + " " + y2 )
-	    .attr("class", "chLine");		    
-
-//ch-ex
-	svg.append("path")
-		.attr("d", "M" + exX + " " + y2 + " " + "C 150 380, 450 380," 
-			+ chX + " " + y2 )
-	    .attr("class", "chLine");		   
+	//ch-ex
+	chEx = interaction[0]["chEx"];
+	for (i = 0; i < interaction[0]["chEx"] ; i++) { 
+		svg.append("path")
+			.attr("d", "M" + exX + " " + y2 + " " 
+				+ "C 150 " + (450 - 80/chEx*i)+ ", 450 " + (450 - 80/chEx*i)+ "," 
+				+ chX + " " + y2 )
+		    .attr("class", "chLine");
+	}    
 
 });
 
